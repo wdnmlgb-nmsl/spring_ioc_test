bd3cf814c01900e7c546789bef3daef59424f96f
wyf <ray335445118@163.com>
2025-03-12 15:04:24 +0800

    修改

--- src/main/java/com/itheima/factory/MyBeanFactory3.java
+++ src/main/java/com/itheima/factory/MyBeanFactory3.java
@@ -5,7 +5,7 @@ import com.itheima.dao.impl.UserDaoImpl;
 import org.springframework.beans.factory.FactoryBean;
 
 //实现FactoryBean规范延迟实例化Bean，单例，创建容器时就实例化了BeanFactory并存储在了FactoryBeanObjectCache中，当需要获取Bean时再通过工厂的getObject去实例对象
-public class MyBeanFactory3 implements FactoryBean {
+public class MyBeanFactory3 implements FactoryBean<UserDao> {
 
     @Override
     public UserDao getObject() throws Exception {
